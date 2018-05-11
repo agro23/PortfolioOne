1dae634c6a0714cda15be4394d1c77f740644977
Andy Grossberg <andy.grossberg@gmail.com>
2018-05-11 11:25:05 -0700

    update README.md and work on API

---
+++ Portfolio/Models/StarredProject.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Portfolio.Models
+{
+    public class StarredProject
+    {
+        public string name { get; set; }
+        public string html_url { get; set; }
+
+        // LIKE TO HAVE THESE BUT THEY'LL HAVE TO BE PARSED SEPARATELY
+        public List<string> Stargazers { get; set; } // "stargazers_url": "https://api.github.com/repos/agro23/xrystal/stargazers",
+
+    }
+}
+/*
+
+{
+            "id": 128805517,
+            "name": "xrystal",
+            "full_name": "agro23/xrystal",
+            "owner": {
+                "login": "agro23",
+                "id": 15318517,
+                "avatar_url": "https://avatars2.githubusercontent.com/u/15318517?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/agro23",
+                "html_url": "https://github.com/agro23",
+                "followers_url": "https://api.github.com/users/agro23/followers",
+                "following_url": "https://api.github.com/users/agro23/following{/other_user}",
+                "gists_url": "https://api.github.com/users/agro23/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/agro23/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/agro23/subscriptions",
+                "organizations_url": "https://api.github.com/users/agro23/orgs",
+                "repos_url": "https://api.github.com/users/agro23/repos",
+                "events_url": "https://api.github.com/users/agro23/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/agro23/received_events",
+                "type": "User",
+                "site_admin": false
+            },
+            "private": false,
+            "html_url": "https://github.com/agro23/xrystal",
+            "description": "Epicodus Week 5 Angular project",
+            "fork": false,
+            "url": "https://api.github.com/repos/agro23/xrystal",
+            "forks_url": "https://api.github.com/repos/agro23/xrystal/forks",
+            "keys_url": "https://api.github.com/repos/agro23/xrystal/keys{/key_id}",
+            "collaborators_url": "https://api.github.com/repos/agro23/xrystal/collaborators{/collaborator}",
+            "teams_url": "https://api.github.com/repos/agro23/xrystal/teams",
+            "hooks_url": "https://api.github.com/repos/agro23/xrystal/hooks",
+            "issue_events_url": "https://api.github.com/repos/agro23/xrystal/issues/events{/number}",
+            "events_url": "https://api.github.com/repos/agro23/xrystal/events",
+            "assignees_url": "https://api.github.com/repos/agro23/xrystal/assignees{/user}",
+            "branches_url": "https://api.github.com/repos/agro23/xrystal/branches{/branch}",
+            "tags_url": "https://api.github.com/repos/agro23/xrystal/tags",
+            "blobs_url": "https://api.github.com/repos/agro23/xrystal/git/blobs{/sha}",
+            "git_tags_url": "https://api.github.com/repos/agro23/xrystal/git/tags{/sha}",
+            "git_refs_url": "https://api.github.com/repos/agro23/xrystal/git/refs{/sha}",
+            "trees_url": "https://api.github.com/repos/agro23/xrystal/git/trees{/sha}",
+            "statuses_url": "https://api.github.com/repos/agro23/xrystal/statuses/{sha}",
+            "languages_url": "https://api.github.com/repos/agro23/xrystal/languages",
+            "stargazers_url": "https://api.github.com/repos/agro23/xrystal/stargazers",
+            "contributors_url": "https://api.github.com/repos/agro23/xrystal/contributors",
+            "subscribers_url": "https://api.github.com/repos/agro23/xrystal/subscribers",
+            "subscription_url": "https://api.github.com/repos/agro23/xrystal/subscription",
+            "commits_url": "https://api.github.com/repos/agro23/xrystal/commits{/sha}",
+            "git_commits_url": "https://api.github.com/repos/agro23/xrystal/git/commits{/sha}",
+            "comments_url": "https://api.github.com/repos/agro23/xrystal/comments{/number}",
+            "issue_comment_url": "https://api.github.com/repos/agro23/xrystal/issues/comments{/number}",
+            "contents_url": "https://api.github.com/repos/agro23/xrystal/contents/{+path}",
+            "compare_url": "https://api.github.com/repos/agro23/xrystal/compare/{base}...{head}",
+            "merges_url": "https://api.github.com/repos/agro23/xrystal/merges",
+            "archive_url": "https://api.github.com/repos/agro23/xrystal/{archive_format}{/ref}",
+            "downloads_url": "https://api.github.com/repos/agro23/xrystal/downloads",
+            "issues_url": "https://api.github.com/repos/agro23/xrystal/issues{/number}",
+            "pulls_url": "https://api.github.com/repos/agro23/xrystal/pulls{/number}",
+            "milestones_url": "https://api.github.com/repos/agro23/xrystal/milestones{/number}",
+            "notifications_url": "https://api.github.com/repos/agro23/xrystal/notifications{?since,all,participating}",
+            "labels_url": "https://api.github.com/repos/agro23/xrystal/labels{/name}",
+            "releases_url": "https://api.github.com/repos/agro23/xrystal/releases{/id}",
+            "deployments_url": "https://api.github.com/repos/agro23/xrystal/deployments",
+            "created_at": "2018-04-09T16:55:50Z",
+            "updated_at": "2018-05-11T15:27:20Z",
+            "pushed_at": "2018-04-24T00:07:44Z",
+            "git_url": "git://github.com/agro23/xrystal.git",
+            "ssh_url": "git@github.com:agro23/xrystal.git",
+            "clone_url": "https://github.com/agro23/xrystal.git",
+            "svn_url": "https://github.com/agro23/xrystal",
+            "homepage": null,
+            "size": 116789,
+            "stargazers_count": 2,
+            "watchers_count": 2,
+            "language": "TypeScript",
+            "has_issues": true,
+            "has_projects": true,
+            "has_downloads": true,
+            "has_wiki": true,
+            "has_pages": false,
+            "forks_count": 1,
+            "mirror_url": null,
+            "archived": false,
+            "open_issues_count": 0,
+            "license": null,
+            "forks": 1,
+            "open_issues": 0,
+            "watchers": 2,
+            "default_branch": "master",
+            "score": 1
+        }
+
+ */
---
+++ Portfolio/README.md
@@ -0,0 +1,183 @@
+﻿# Portfolio
+
+### By Andy Grossberg
+
+## Description
+A .Net portfolio with blog and user comments as well as a Projects page with the top three most starred Github repos.
+
+## Rules
+
+This is a personal portfolio site. I'll be building this same project for the next two Fridays.
+
+The site must include:
+
+* A landing page featuring an image of yourself or an image that expresses your style.
+* A blog section featuring your posts. You are not required to write any posts for this project.
+
+* Users should be able to:
+  - comment on specific blog posts I’ve made.
+  - see all previous comments left on blog posts. (All comments should have body and author fields)
+
+* I should be able to log on/off as an administrator for my portfolio.
+  - I can choose to either seed my database with an admin username and password, or include a registration page to register.
+
+* At the admin level I should be able to:
+  - Create, Read, Update, and Delete blog entries.
+    * (I will Use AJAX for the GET methods for this)
+  - Create comments and view and remove comments anywhere on the site.
+
+* My site must include:
+    - My work experience
+    - My relevant skills
+    - My education
+    - How I got into coding
+    - A personal story and interests
+    - Contact information (link to GH, linkedin)
+    - My Your personal and work projects
+
+## Objectives
+
+**Phase One Objectives**
+* Code continues to meet all of the standards from last week.
+
+* Application works as expected.
+
+* Application uses Identity properly. Requested login/logoff functionality is present.
+
+* Application uses AJAX successfully. Requested AJAX functionality is present and working.
+
+* Code is clear, easy to understand, and well-refactored.
+
+* Project is in a polished, portfolio-quality state.
+
+* Required functionality was present by Friday deadline.
+
+* Project demonstrate understanding of this week's concepts. If prompted, you can discuss your code with an instructor using correct terminology.
+
+**Phase Two Objectives**
+
+* Build out projects section
+
+* Add Github API connection
+
+* Display top three most-starred apps on Github
+
+* Return to correct and finish incomplete or malfunctioning Phase One Objectives
+
+## User Stories
+There will ultimately be two levels of user for this site: Administrator and Reader. At the start however every user is an Admin.
+
+**All Users**
+* Read Blog Posts
+
+* Comment on Blog Posts
+
+**Admin**
+* Create, Read, Update and Delete posts and Comments
+
+* Maybe Remove or Block users
+
+## Specifications
+
+* Blog Data Model is created. -- X
+
+* User Data Model is created. -- X
+
+* Comment Data Model is created. -- X
+
+* Add Author name to Comments
+
+* ??? Add DateTime to BlogPosts
+
+* Migrate database to update BlogPosts and Comments
+
+* Site MVC Built.
+  - Home Controller
+  - Home View
+  - Login/Register Controller
+  - Login/ Register View
+  - Blog Controller
+  - Blog View
+
+* Landing page created (updated from Home View). -- X
+
+* Landing page features an image that expresses my style. -- X
+
+* Blog database migration -- X
+
+* Authentication via Nav bar -- !!!
+
+* Fix Authorization!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+* LOGOUT USING AJAX <---------------------------------------------------------
+
+* Blog Entries can be READ
+
+* Blog Entries can be created, retrieved, updated, and removed from the database.
+
+* ?Users can be created, retrieved, updated, and removed from the database.
+
+* Blog Entry Comments can be read 
+
+* Blog Entry Comments can be added by Users.
+
+* Blog Entry Comments can be CREATED, READ, UPDATED, and DELETED by Admin
+
+
+* Make API call to github
+
+* Parse JSON results into StarredProject object
+
+* Display Results on Projects page
+
+
+* User Authorization Levels
+
+* Only Admin can post blog posts
+
+* Style and personalize site.
+
+* Refactor code as needed.
+
+## Methodology and Comments
+
+Last week we studied Authentication and AJAX and so I'm using those class projects as inspiration for the back end of the app. The last two-week project had reviews and those are the same as comments so I will use something similar in structure.
+
+This week I have completely changed how I am going to do my blog posts and comments. I will complete this after I complete the API call.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Javascript
+* jQuery
+* C#
+* .NET (ASP) Core
+* mySQL
+
+## Dependencies and plugins
+
+**Dependencies**
+TBD
+
+## Setup/Installation Requirements
+TBD
+
+## Future expansion
+There is room to separate the Admin from Users but not in the short term. In order to keep updating this site I will have to do so.
+
+## Known Bugs and Issues
+
+There are no known bugs or issues at this time.
+
+## Support and contact details
+
+@using (Html.BeginForm("LogOff", "Account"))
+    {
+        <input type="submit" class="btn btn-default" value="Log out" />
+    }
+
+**Contact the author at andy.grossberg@gmail.com**
+
+## License
+Licensed for use under the GNU GPL. (c) 2018 Andy Grossberg
